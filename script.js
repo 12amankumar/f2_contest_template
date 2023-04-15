da51bd47eb789b81d1b672d8a66c37f3144393ce
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-04-15 14:55:38 +0530

    commit

--- script.js
+++ script.js
@@ -8,20 +8,50 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((employee)=>{
+   if( employee.profession=="developer"){
+    console.log(employee);
+   }
+   
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
-}
-
+  arr.forEach((employee)=>{
+    if( employee.profession=="developer"){
+     console.log(employee);
+    }
+    
+   })
+ }
+ 
 function addData() {
   //Write your code here, just console.log
-}
-
+  let employee=[];
+  employee.unshift({id:4,name:"aman",age:"20",profession: "developer"});
+  console.log(employee);
+  }
 function removeAdmin() {
   //Write your code here, just console.log
+//  let i=0;
+let employee=admin;
+ if(employee=="admin"){
+console.log("Aman")
+ }
+ 
 }
 
+
 function concatenateArray() {
-  //Write your code here, just console.log
+  //Write your code here, just console.log 
+   let aman = [
+    { id: 5, name: "rohan", age: "18",profession: "admin" },
+    { id: 6, name: "mohan", age: "24",profession: "developer"},
+    { id: 7, name: "kishan", age: "22",profession: "admin"},
+  ];
+  let ayushsir=arr.concat(aman);
+  console.log(ayushsir);
+  
 }
+
